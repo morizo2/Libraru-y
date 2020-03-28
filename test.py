97f042a262a03a79aed64844ab004857d98e0915
morizo <kei5exvs@gmail.com>
2020-03-29 03:00:34 +0900

    invtable

---
+++ test.py
@@ -0,0 +1,18 @@
+class InverseTable:
+    __slots__ = ['In', 'fac', 'Ifac']
+
+    def __init__(self, mod, maxnum):
+        self.In = [0, 1]; self.fac = [1, 1]; self.Ifac = [1, 1]
+        for i in range( 2, maxnum + 1 ):
+            self.In.append( ( -self.In[mod % i] * (mod//i) ) % mod )
+            self.fac.append( ( self.fac[-1] * i ) % mod )
+            self.Ifac.append( (self.Ifac[-1] * self.In[-1]) % mod )
+        
+    def cmb(self, n, r):
+        if ( r<0 or r>n ): return 0
+        r = min(r, n-r)
+        return self.fac[n] * self.Ifac[r] * self.Ifac[n-r] % mod
+    
+    def per(self, n, r):
+        if ( r<0 or r>n ): return 0
+        return self.fac[n] * self.Ifac[n-r] % mod
